96219d34dac65a67ff87b58f5b3921b37e00d7b5
marti <marti@PC-Martin>
2020-04-07 21:49:13 +0200

    organize imports

--- k8s-be/src/main/java/ch/martin/k8s/be/GreetingController.java
+++ k8s-be/src/main/java/ch/martin/k8s/be/GreetingController.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
--- k8s-bff/src/main/java/ch/martin/k8s/bff/BackendClient.java
+++ k8s-bff/src/main/java/ch/martin/k8s/bff/BackendClient.java
@@ -1,10 +1,7 @@
 package ch.martin.k8s.bff;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**

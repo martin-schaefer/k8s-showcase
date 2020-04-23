829b80d29d5d6a4d119ccc850bd3f13d700db591
marti <marti@PC-Martin>
2020-04-23 20:47:31 +0200

    Added terraform modules for Spring Boot Apps

--- tf-deployment/main.tf
+++ tf-deployment/main.tf
@@ -1,67 +1,24 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Terraform deployment for k8s-showcasw
+# Terraform deployment for k8s-showcase
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 terraform {
   required_version = ">= 0.12"
 }
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# The Kubernetes provider
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 provider "kubernetes" {
   config_context = var.kubectl_config_context_name
   config_path    = var.kubectl_config_path
 }
 
-resource "kubernetes_deployment" "k8s-be" {
-  metadata {
-    name = "k8s-be"
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "k8s-be"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "k8s-be"
-          fluentd-log-format = "spring-boot-json"
-        }
-        annotations = {
-          "prometheus.io/scrape" = "true"
-          "prometheus.io/path" = "/actuator/prometheus"
-          "prometheus.io/port" = "8010"
-        }
-      }
-      spec {
-        container {
-          name  = "k8s-be-container"
-          image = "gcr.io/handy-zephyr-272321/k8s-be:latest"
-          port {
-            name = "service-port"
-            container_port = 80
-          }
-          port {
-            name = "management-port"
-            container_port = 8010
-          }
-          readiness_probe {
-            http_get {
-              path = "/actuator/health"
-              port = 8010
-            }
-            timeout_seconds = 5
-            period_seconds = 30
-            success_threshold = 1
-            failure_threshold = 1
-          }
-        }
-      }
-    }
-  }
+module "k8s-be" {
+  source = "./spring-boot-app"
+  app_name = "k8s-be"
+  node_port = 30001
+}
+
+module "k8s-bff" {
+  source = "./spring-boot-app"
+  app_name = "k8s-bff"
+  node_port = 30002
 }
---
+++ tf-deployment/spring-boot-app/module.tf
@@ -0,0 +1,92 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Module for Spring Boot app standard deployment
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+variable "app_name" {
+  type = string
+}
+
+variable "node_port" {
+  type = number
+}
+
+resource "kubernetes_deployment" "spring-boot-app-deployment" {
+  metadata {
+    name = var.app_name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = var.app_name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = var.app_name
+          fluentd-log-format = "spring-boot-json"
+        }
+        annotations = {
+          "prometheus.io/scrape" = "true"
+          "prometheus.io/path" = "/actuator/prometheus"
+          "prometheus.io/port" = "8010"
+        }
+      }
+      spec {
+        container {
+          name  = "${var.app_name}container"
+          image = "gcr.io/handy-zephyr-272321/${var.app_name}:latest"
+          port {
+            name = "service-port"
+            container_port = 80
+          }
+          port {
+            name = "management-port"
+            container_port = 8010
+          }
+          readiness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8010
+            }
+            timeout_seconds = 5
+            period_seconds = 30
+            success_threshold = 1
+            failure_threshold = 1
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "spring-boot-app-service" {
+  metadata {
+    name = var.app_name
+  }
+  spec {
+    selector = {
+      app = var.app_name
+    }
+    port {
+      port = 80
+      node_port = var.node_port
+    }
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_service" "spring-boot-app-management" {
+  metadata {
+    name = "${var.app_name}-management"
+  }
+  spec {
+    selector = {
+      app = var.app_name
+    }
+    port {
+      port = 8010
+    }
+  }
+}

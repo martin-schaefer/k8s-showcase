b9649de1aefdbad214c8888f4c721ba9308c9cbb
Martin Schäfer <martin.schaefer.14@outlook.com>
2020-06-27 16:56:34 +0200

    add log argument 'messageId' as distinct elastisearch field

--- k8s-be/src/main/java/ch/martin/k8s/be/GreetingController.java
+++ k8s-be/src/main/java/ch/martin/k8s/be/GreetingController.java
@@ -1,5 +1,7 @@
 package ch.martin.k8s.be;
 
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.argument.StructuredArguments;
 
 @RestController
 @RefreshScope
@@ -18,7 +21,8 @@ public class GreetingController {
 
 	private final String appName;
 	private final String message;
-
+	private int messageId = 1000;
+	
 	public GreetingController(@Value("${spring.application.name}") @NonNull String appName,
 			@Value("${k8s-be.message}") @NonNull String message) {
 		this.appName = appName;
@@ -28,7 +32,7 @@ public class GreetingController {
 	@GetMapping("/")
 	public GreetingDto getGreeting(@RequestParam("name") String name) {
 		String returnedMessage = message + " to " + name + " from the K8S backend! Time: " + LocalDateTime.now();
-		log.info("{} will return message: {}", appName, message);
+		log.info("{} will return message: {}, messageId = {}", appName, message, keyValue("messageId", messageId++));
 		log.error("Just another ugly error. But the stack trace will save your day.", new Exception("An Exception"));
 		return new GreetingDto(returnedMessage);
 	}
